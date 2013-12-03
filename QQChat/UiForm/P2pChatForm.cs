1d59718d0a0e0107128f23b7de7257e68d0a0b04
yinchuandong <yincd520@sina.com>
2013-12-03 10:39:33 +0800

    完成了聊天界面的输入和显示

--- QQChat/UiForm/P2pChatForm.cs
+++ QQChat/UiForm/P2pChatForm.cs
@@ -42,16 +42,28 @@ namespace QQChat.UiForm
             }
         }
 
-        string imgName = "";
+        string faceName = "";//选择的表情的名字
+        //表情框选择了表情之后的事件
         void faceForm_AddFace(object sender, SelectFaceArgs e)
         {
-            
+            this.faceName = e.Img.FullName.Replace(Application.StartupPath + "\\", "");
+            this.sendRichBox.InsertImage(e.Img.Image);
         }
 
+        //显示表情button按钮
         private void faceBtn_Click(object sender, EventArgs e)
         {
             Point pt = this.PointToScreen(new Point(((Button)sender).Left, ((Button)sender).Height + 5));
             this.FaceForm.Show(pt.X, pt.Y, ((Button)sender).Height);
+        }
+
+        //发送消息的button
+        private void sendBtn_Click(object sender, EventArgs e)
+        {
+            string msg = sendRichBox.Rtf;
+            sendRichBox.Text = String.Empty;
+            messageRichBox.AppendRtf(msg);
+            messageRichBox.ScrollToCaret();
         }
     }
 }
