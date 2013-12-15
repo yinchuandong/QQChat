1cdb2f51d125a0418853cd69fb69c7ea2b810fd2
kelink <1030041097@qq.com>
2013-12-15 17:11:18 +0800

    解决冲突

--- QQChat/UiForm/GroupListForm.cs
+++ QQChat/UiForm/GroupListForm.cs
@@ -58,10 +58,6 @@ namespace QQChat.UiForm
             groupListBox.Items.Add(listItem);
         }
 
-        //创建新群
-        private void newGroup_button_Click(object sender, EventArgs e)
-        {
-            new AddChatRoomForm(user).Visible = true;
-        }
+        
     }
 }
