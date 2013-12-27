8cf6edbe2dc9e000a0a3d1eb8465589e16d6ccc6
kelink <1030041097@qq.com>
2013-12-27 14:30:26 +0800

    解决冲突

--- QQChat/UiForm/GroupChatForm.Designer.cs
+++ QQChat/UiForm/GroupChatForm.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GroupChatForm));
             this.GroupChat_Output = new Widget._RichTextBox.ExRichTextBox();
             this.GroupChat_Input = new Widget._RichTextBox.ExRichTextBox();
@@ -41,20 +40,24 @@
             // 
             // GroupChat_Output
             // 
+            this.GroupChat_Output.HiglightColor = Widget._RichTextBox.RtfColor.White;
             this.GroupChat_Output.Location = new System.Drawing.Point(-1, 81);
             this.GroupChat_Output.Name = "GroupChat_Output";
             this.GroupChat_Output.Size = new System.Drawing.Size(499, 215);
             this.GroupChat_Output.TabIndex = 0;
             this.GroupChat_Output.Text = "";
+            this.GroupChat_Output.TextColor = Widget._RichTextBox.RtfColor.Black;
             this.GroupChat_Output.TextChanged += new System.EventHandler(this.GroupChat_Output_TextChanged);
             // 
             // GroupChat_Input
             // 
+            this.GroupChat_Input.HiglightColor = Widget._RichTextBox.RtfColor.White;
             this.GroupChat_Input.Location = new System.Drawing.Point(0, 328);
             this.GroupChat_Input.Name = "GroupChat_Input";
             this.GroupChat_Input.Size = new System.Drawing.Size(498, 103);
             this.GroupChat_Input.TabIndex = 1;
             this.GroupChat_Input.Text = "";
+            this.GroupChat_Input.TextColor = Widget._RichTextBox.RtfColor.Black;
             // 
             // headPicBox
             // 
@@ -68,9 +71,10 @@
             // 
             this.nameTxt.AutoSize = true;
             this.nameTxt.BackColor = System.Drawing.Color.Transparent;
-            this.nameTxt.Location = new System.Drawing.Point(93, 2);
+            this.nameTxt.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.nameTxt.Location = new System.Drawing.Point(93, 52);
             this.nameTxt.Name = "nameTxt";
-            this.nameTxt.Size = new System.Drawing.Size(41, 12);
+            this.nameTxt.Size = new System.Drawing.Size(55, 21);
             this.nameTxt.TabIndex = 3;
             this.nameTxt.Text = "label1";
             // 
--- Util/FileUtil.cs
+++ Util/FileUtil.cs
@@ -1,13 +1,69 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+
+
 
 namespace Util
 {
     public class FileUtil
     {
-        private String basePath = AppDomain.CurrentDomain.BaseDirectory;
+        public static String basePath = AppDomain.CurrentDomain.BaseDirectory;
+
+       //保存到本地文件
+        public static void  save2File(int guestId,string userName,string content)
+        {
+            //创建一个文件流，用以写入或者创建一个StreamWriter 
+            string dirPath = basePath + userName;
+            string path = guestId.ToString() + ".txt";
+            //创建文件夹
+            if (!Directory.Exists(dirPath))//判断文件夹是否已经存在
+            {
+                DirectoryInfo dir= Directory.CreateDirectory(dirPath);//创建文件夹
+                StreamWriter sw = File.CreateText(dirPath + "\\" + guestId.ToString() + ".txt");
+                sw.WriteLine(content);
+                sw.Close();
+            }         
+            else 
+            {
+                FileStream fs = new FileStream(dirPath + "\\" + guestId.ToString() + ".txt", FileMode.OpenOrCreate, FileAccess.Write);
+                StreamWriter m_streamWriter = new StreamWriter(fs);
+                m_streamWriter.Flush();  // 使用StreamWriter来往文件中写入内容
+                m_streamWriter.BaseStream.Seek(0, SeekOrigin.Begin);
+                m_streamWriter.Write(content);    // 把内容写入文件
+                m_streamWriter.Close(); //关闭此文件  m_streamWriter.Flush ( ) ;
+            }                
+        }
+
+        public static string readFromFile(int guestId, string userName)
+        {  //读取文档
+
+            string dirPath = basePath + userName;
+            string fileName = guestId.ToString() + ".txt";
+            string path = dirPath + "\\" + guestId.ToString() + ".txt";
+            string content="";
+            FileStream fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Read);
+           StreamReader m_streamReader = new StreamReader(fs);  //使用StreamReader类来读取文件
+           m_streamReader.BaseStream.Seek(0, SeekOrigin.Begin);  // 从数据流中读取每一行，直到文件的最后一行，并在richTextBox1中显示出内容   
+
+           string strLine = m_streamReader.ReadLine();
+           while (strLine != null)
+           { 
+             content += strLine;
+             strLine = m_streamReader.ReadLine();
+           }
+           //关闭此StreamReader对象
+           m_streamReader.Close();
+          return content;
+        }
+        //格式化对象
+
 
     }
 }
